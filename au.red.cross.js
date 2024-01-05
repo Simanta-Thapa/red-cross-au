97a9903e49b5448dae15c6ab9d8aa0e547b94df3
SIMANTA THAPA <megadsoul2005@gmail.com>
2024-01-05 15:02:44 +0545

    first commit

---
+++ au.red.cross.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+$(".NavBarSeccond li a").mouseenter(function(){
+
+
+  $(".GethelpPopUp").removeClass("active");
+   $("#" + $(this).attr("data-tab")).addClass("active");
+$("#first").addClass("index");
+
+
+
+//    $("#tabs li a").click(function(){
+//     $("#tabs li a").removeClass("active");
+//     $(this).addClass("active");
+//     $(".tab").removeClass("active")
+//     $("#" + $(this).attr("data-tab")).addClass("active");
+//     });
+
+
+});
+
+
+// $(".bottomNavBar").mouseleave(function(){
+//     $("#" + $(this).attr("data-tab")).removeClass("active");
+// })
+
+$(".bottomNavBar").mouseleave(function(){
+
+    $("#" + $(this).attr("data-tab")).removeClass("active");
+})
+
+
+$(".GethelpPopUp").mouseleave(function(){
+
+    $(".GethelpPopUp").removeClass("active");
+    $("#first").removeClass("index");
+})
+})
+
+
+
